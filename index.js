2ab6d15d655372f325f59612e959bafa108400e2
rsbhargav <37236514+RsBhargav@users.noreply.github.com>
2021-12-01 10:52:05 +0530

    added users

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy 
 const keys = require('./config/keys')
+require('./models/user')
+require('./services/passport')
 
 const app = express()
-
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken)
-})
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 )
-
-app.get('/auth/google', 
-passport.authenticate('google',{
-    scope: ['profile', 'email']
-}))
+app.use(passport.initialize())
+app.use(passport.session())
+require('./routes/authRoutes')(app)
+mongoose.connect(keys.mongoURI)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
