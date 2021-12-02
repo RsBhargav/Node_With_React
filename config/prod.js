12fc9cb362ebb298a3145f4ce082255b6bcfb6d0
rsbhargav <37236514+RsBhargav@users.noreply.github.com>
2021-12-02 16:30:40 +0530

    added prod keys

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+//prod
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID
+    '674197111336-m1sg7gn9s7vtidc4rdrsl1qutj1j3ekc.apps.googleusercontent.com',
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    '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',
+    mongoURI: process.env.MONGO_URI'mongodb+srv://rsbhargav:ril.sai%402021@cluster0.guhum.mongodb.net/surveyKingDB?retryWrites=true&w=majority',
+    cookieKey: process.env.COOKIE_KEY,
+};  
