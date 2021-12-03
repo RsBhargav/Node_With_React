25c36262adc3d4dbf4a0d964b20840933fea052f
rsbhargav <37236514+RsBhargav@users.noreply.github.com>
2021-12-03 22:09:41 +0530

    update auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,8 +12,8 @@ module.exports = app => {
     app.get('/api/logout', (req, res) => {
         req.logout()
         res.send(req.user)
-    })
+    });
     app.get('/api/current_user', (req, res) => {
         res.send(req.user)
-    })
+    });
 }
